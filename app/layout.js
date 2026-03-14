62ef32820dab3640dbf1d91f898dc0a47ec1989e
alfmgvg <72279857+alfmgvg@users.noreply.github.com>
2026-03-14 07:26:30 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
+import './globals.css';
+
 export const metadata = {
-  title: 'Cirujano Maxilofacial en Sabadell',
-  description: 'Muelas del juicio e implantes dentales en Sabadell',
+  title: 'Cirujano Maxilofacial en Sabadell | Muelas del juicio e implantes dentales',
+  description:
+    'Unidad de Cirugía Oral y Maxilofacial en el Hospital Quirónsalud del Vallès. Especialistas en muelas del juicio, cordales incluidos e implantes dentales en Sabadell y el Vallès.',
 };
 
 export default function RootLayout({ children }) {
